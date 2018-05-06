811dac038b5394adc015d3c5dc0b524589c31e0b
XenonNSMB <git@xenonnsmb.com>
2018-05-06 14:20:51 -0500

    use cxfreeze on windows instead of py2exe

--- setup.py
+++ setup.py
@@ -8,14 +8,19 @@ if (sys.platform == 'darwin'):
 		options=dict(py2app=dict(argv_emulation=True))
 	)
 elif (sys.platform == 'win32'):
+	from cx_Freeze import setup, Executable
 	extra_options = dict(
-		setup_requires=["py2exe"]
+		setup_requires=["cx_Freeze"],
+		options = {"build_exe": build_exe_options},
+		executables = [Executable(main, base="Win32GUI")])
 	)
 else:
 	extra_options = dict(
 		scripts=[main]
 	)
 setup(
+	name = "ezrp",
+	version = "1.0.0-alpha2"
     app=[main],
 	**extra_options
 )
